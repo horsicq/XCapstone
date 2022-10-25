d72dbc6b45bf5279fcad0f5a82601c5d14d92281
horsicq <horsicq@gmail.com>
2022-10-25 13:44:34 +0200

    Fix: 2022-10-25

--- xcapstone.h
+++ xcapstone.h
@@ -75,7 +75,7 @@ public:
     static void printEnabledArchs();
 
 private:
-    static const int N_OPCODE_SIZE=15; // mb TODO rename
+    static const int N_OPCODE_SIZE=15; // mb TODO rename set/get
 };
 
 #endif // XCAPSTONE_H
