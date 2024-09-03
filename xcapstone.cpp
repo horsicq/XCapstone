ef118071d184b5c5fcbf24070e8dcb32ae5273d3
horsicq <horsicq@gmail.com>
2024-09-03 19:34:09 +0200

    Update file(s): 2024-09-03

--- xcapstone.cpp
+++ xcapstone.cpp
@@ -160,6 +160,8 @@ XCapstone::DISASM_RESULT XCapstone::disasm_ex(csh handle, XBinary::DM disasmMode
 
     result.nAddress = nAddress;
 
+    XBinary::DMFAMILY dmFamily = XBinary::getDisasmFamily(disasmMode);
+
     if (handle) {
         cs_insn *pInsn = nullptr;
 
@@ -180,32 +182,15 @@ XCapstone::DISASM_RESULT XCapstone::disasm_ex(csh handle, XBinary::DM disasmMode
                         for (qint32 j = 0; j < pInsn->detail->x86.op_count; j++) {
                             // TODO mb use groups
                             if (pInsn->detail->x86.operands[j].type == X86_OP_IMM) {
-                                switch (pInsn->id) {
-                                    case X86_INS_LOOP:
-                                    case X86_INS_LOOPE:
-                                    case X86_INS_LOOPNE:
-                                    case X86_INS_JAE:
-                                    case X86_INS_JA:
-                                    case X86_INS_JBE:
-                                    case X86_INS_JB:
-                                    case X86_INS_JCXZ:
-                                    case X86_INS_JECXZ:
-                                    case X86_INS_JE:
-                                    case X86_INS_JGE:
-                                    case X86_INS_JG:
-                                    case X86_INS_JLE:
-                                    case X86_INS_JL:
-                                    case X86_INS_JNE:
-                                    case X86_INS_JNO:
-                                    case X86_INS_JNP:
-                                    case X86_INS_JNS:
-                                    case X86_INS_JO:
-                                    case X86_INS_JP:
-                                    case X86_INS_JRCXZ:
-                                    case X86_INS_JS: result.relType = RELTYPE_JMP_COND; break;
-                                    case X86_INS_CALL: result.relType = RELTYPE_CALL; break;
-                                    case X86_INS_JMP: result.relType = RELTYPE_JMP_UNCOND; break;
-                                    default: result.relType = RELTYPE_JMP;  // TODO
+
+                                if (isCallOpcode(dmFamily, pInsn->id)) {
+                                    result.relType = RELTYPE_CALL;
+                                } else if (isJumpOpcode(dmFamily, pInsn->id)) {
+                                    result.relType = RELTYPE_JMP_UNCOND;
+                                } else if (isCondJumpOpcode(dmFamily, pInsn->id)) {
+                                    result.relType = RELTYPE_JMP_COND;
+                                } else {
+                                    result.relType = RELTYPE_JMP;
                                 }
 
                                 result.nXrefToRelative = pInsn->detail->x86.operands[j].imm;
