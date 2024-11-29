9c5d39ed6887e730429ea1c0a5bd9c793a0944ca
horsicq <horsicq@gmail.com>
2024-11-29 07:49:53 +0100

    Update file(s): 2024-11-29

--- xcapstone.cmake
+++ xcapstone.cmake
@@ -1,9 +1,13 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/Capstone/src/include/)
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
+if (NOT DEFINED XBINARY_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
+    set(XCAPSTONE_SOURCES ${XCAPSTONE_SOURCES} ${XBINARY_SOURCES})
+endif()
 
 set(XCAPSTONE_SOURCES
+    ${XCAPSTONE_SOURCES}
     ${XBINARY_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xcapstone.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xcapstone.h
