a0649ab6c5c95e9f38d4ed0a029a682cc3a70f4f
horsicq <horsicq@gmail.com>
2025-01-15 06:10:08 +0100

    Update file(s): 2025-01-15

--- xcapstone.cpp
+++ xcapstone.cpp
@@ -110,7 +110,6 @@ cs_err XCapstone::openHandle(XBinary::DM disasmMode, csh *pHandle, bool bDetails
     else if (disasmMode == XBinary::DM_MOS65XX) result = cs_open(CS_ARCH_M680X, cs_mode(CS_ARCH_MOS65XX), pHandle);
     else if (disasmMode == XBinary::DM_BPF_LE) result = cs_open(CS_ARCH_BPF, cs_mode(CS_MODE_BPF_CLASSIC | CS_MODE_LITTLE_ENDIAN), pHandle);
     else if (disasmMode == XBinary::DM_BPF_BE) result = cs_open(CS_ARCH_BPF, cs_mode(CS_MODE_BPF_CLASSIC | CS_MODE_BIG_ENDIAN), pHandle);
-    // else if (disasmMode == XBinary::DM_CUSTOM_MACH_REBASE) result = CS_ERR_OK;
     // TODO Check more
 
     if (result == CS_ERR_OK) {
